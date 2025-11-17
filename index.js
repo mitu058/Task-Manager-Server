581ce7afdc69965c16ae2d1d7c9e8f1ac530a227
mitu058 <mitum9796@gmail.com>
2025-11-17 22:11:19 +0600

    install dotenv

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
@@ -88,5 +90,6 @@ app.get("/", (req, res) => {
   res.send("Task Manager Server Running");
 });
 
-// ❗ IMPORTANT FOR VERCEL
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
